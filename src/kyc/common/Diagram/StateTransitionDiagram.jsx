59c5d75ce60653290e46bf739b1f69a5b0466993
chlwlgns524 <chlwlgns524@naver.com>
2024-02-17 16:04:10 +0900

    [feat] - 상태 전이 다이어 그램 개발

---
+++ src/kyc/common/Diagram/StateTransitionDiagram.jsx
@@ -0,0 +1,63 @@
+import styles from "./StateTransitionDiagram.module.css";
+import PropTypes from "prop-types";
+
+export default function StateTransitionDiagram({current}) {
+    const states = [
+        {
+            label: "서비스 신청",
+            image: "abc.png"
+        },
+        {
+            label: "회원 가입",
+            image: "def.png"
+        },
+        {
+            label: "가나다",
+            image: "abc.png"
+        },
+        {
+            label: "라마바",
+            image: "def.png"
+        },
+    ];
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.lines}>
+                {
+                    states
+                        .slice(0, states.length - 1)
+                        .map((state, index) =>
+                            <div key={state.label}
+                                 className={`
+                                 ${styles.line} 
+                                 ${(index + 2) < parseInt(current) ? styles.done : ((index + 2) === parseInt(current) ? styles.ing : "")}
+                                 `}>
+
+                            </div>)
+                }
+            </div>
+            <div className={styles.circles}>
+                {
+                    states
+                        .map((state, index) =>
+                            <div key={state.label} className={styles.circleWrapper}>
+                                <div
+                                    className={`
+                                    ${styles.circle} 
+                                    ${(index + 1) < parseInt(current) ? styles.done : ((index + 1) === parseInt(current) ? styles.ing : "")}
+                                    `}>
+                                    <span className={styles.image}>{state.image}</span>
+                                </div>
+                                <span className={styles.label}>{state.label} {index + 1 === parseInt(current) ? "t": "f"}</span>
+                            </div>
+                        )
+                }
+            </div>
+        </div>
+    )
+}
+
+StateTransitionDiagram.propTypes = {
+    current: PropTypes.string.isRequired,
+}

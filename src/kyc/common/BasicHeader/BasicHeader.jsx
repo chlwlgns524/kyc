d35f688ee3eadfe1d3d314d17893a075e2d4571e
chlwlgns524 <chlwlgns524@naver.com>
2024-02-17 16:04:30 +0900

    [refactor] - 상태 전이 다이어그램 반영

--- src/kyc/common/BasicHeader/BasicHeader.jsx
+++ src/kyc/common/BasicHeader/BasicHeader.jsx
@@ -1,14 +1,16 @@
 import styles from "./BasicHeader.module.css";
 import PropTypes from "prop-types";
 
-export default function BasicHeader({title}) {
+export default function BasicHeader({title, diagram: Diagram}) {
     return (
         <header className={styles.container}>
             <h1 className={styles.title}>{title}</h1>
+            {Diagram && <Diagram/>}
         </header>
     )
 }
 
 BasicHeader.propTypes = {
     title: PropTypes.string.isRequired,
+    diagram: PropTypes.elementType,
 }
--- src/kyc/signup/ServiceRegistration/ServiceRegistration.jsx
+++ src/kyc/signup/ServiceRegistration/ServiceRegistration.jsx
@@ -1,12 +1,16 @@
 import BasicLayout from "../../../layout/BasicLayout/BasicLayout.jsx";
 import BasicHeader from "../../common/BasicHeader/BasicHeader.jsx";
-import ServiceRegisterationForm from "./ServiceRegisterForm/ServiceRegisterationForm.jsx";
+import ServiceRegistrationForm from "./ServiceRegisterForm/ServiceRegisterationForm.jsx";
+import StateTransitionDiagram from "../../common/Diagram/StateTransitionDiagram.jsx";
 
 export default function ServiceRegistration() {
     return (
         <BasicLayout
-            Header={() => <BasicHeader title={"온라인 결제서비스 회원가입"}/>}
-            Body={() => <ServiceRegisterationForm/>}
+            Header={() => <BasicHeader
+                title={"온라인 결제서비스 회원가입"}
+                diagram={() => <StateTransitionDiagram current={'2'}/>}
+            />}
+            Body={() => <ServiceRegistrationForm/>}
         />
     )
 }

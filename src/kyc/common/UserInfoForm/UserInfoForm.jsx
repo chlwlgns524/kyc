3a377a1678639281b5c90b99c7a2ae03bc4c12e6
chlwlgns524 <chlwlgns524@naver.com>
2024-02-19 08:58:29 +0900

    [refactor] - UserInfoForm 디렉토리 변경

--- src/kyc/common/UserInfoForm/UserInfoForm.jsx
+++ src/kyc/common/UserInfoForm/UserInfoForm.jsx
@@ -1,20 +1,19 @@
-import styles from "./UserInfoForm.module.css"
 import UserInput from "./UserInput/UserInput.jsx";
 import PropTypes from "prop-types";
 import TelephoneUserInput from "./UserInput/TelephoneUserInput.jsx";
-import MainForm from "../../../../../components/MainForm/MainForm.jsx";
+import MainForm from "../../../components/MainForm/MainForm.jsx";
+import Matrix from "../../../components/Matrix/Matrix.jsx";
+import UploadUserInput from "./UserInput/UploadUserInput.jsx";
 
-export default function UserInfoForm({user, userInputList, pickUserInput}) {
+export default function UserInfoForm({title, description, referenceValue, userInputList, pickUserInput}) {
     console.log("<UserInfoForm/> rendered!");
-
-    const currentPassword = getCurrentPassword(user);
 
     return (
         <MainForm
-            title={"회원가입"}
-            description={"이후 회원가입 프로세스 진행 및 서비스 관리자 페이지 접속을 위해 필요한 로그인 정보를 생성합니다."}
+            title={title}
+            description={description}
         >
-            <div className={styles.maxtrix}>
+            <Matrix>
                 {
                     userInputList.map((userInput, index) => {
                         switch (userInput.id) {
@@ -30,12 +29,21 @@ export default function UserInfoForm({user, userInputList, pickUserInput}) {
                                     essential={userInput.essential}
                                     pickUserInput={pickUserInput}
                                 />
+                            case 'businessLicense':
+                                return <UploadUserInput
+                                    key={userInput.id}
+                                    id={userInput.id}
+                                    label={userInput.label}
+                                    essential={userInput.essential}
+                                    pickUserInput={userInput.pickUserInput}
+                                    validator={userInput.validator}
+                                />
                             default:
                                 return <UserInput
                                     key={userInput.id}
                                     id={userInput.id}
                                     label={userInput.label}
-                                    referenceValue={currentPassword}
+                                    referenceValue={referenceValue}
                                     validator={userInput.validator}
                                     essential={userInput.essential}
                                     pickUserInput={pickUserInput}
@@ -43,17 +51,15 @@ export default function UserInfoForm({user, userInputList, pickUserInput}) {
                         }
                     })
                 }
-            </div>
+            </Matrix>
         </MainForm>
     )
 }
 
-function getCurrentPassword(user) {
-    return user.find(info => info.id === 'password').value ?? '';
-}
-
 UserInfoForm.propTypes = {
-    user: PropTypes.array,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    referenceValue: PropTypes.string,
     userInputList: PropTypes.array.isRequired,
     pickUserInput: PropTypes.func.isRequired,
 }
